c56096a59cc834338bb42bab2ccde189a43e7278
Aimen Chaib <62136824+aymench15@users.noreply.github.com>
2024-02-25 23:37:29 +0100

    adding map and its features

---
+++ public/js/map.js
@@ -0,0 +1,41 @@
+var map = L.map("mapid").setView([34.45, 5.6], 13);
+
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      var marker;
+      var lat;
+      var long;
+      var onMapClick = (e) => {
+        if (marker) {
+          map.removeLayer(marker);
+          lat = "";
+          long = "";
+        }
+        marker = L.marker(e.latlng).addTo(map);
+        lat = e.latlng.lat.toFixed(6);
+        long = e.latlng.lng.toFixed(6);
+        document.getElementById("coordinates").textContent =
+          "Latitude: " +
+          e.latlng.lat.toFixed(6) +
+          ", Longitude: " +
+          e.latlng.lng.toFixed(6);
+      };
+
+      map.on("click", onMapClick);
+
+      var element;
+      function formatDate(dateString) {
+        const date = new Date(dateString);
+        return date.toLocaleDateString();
+      }
+
+      // Function to extract and format parameter values
+      function getParameterValues(parameterName, timeSeries) {
+        const parameter = timeSeries.parameters.find(
+          (p) => p.name === parameterName
+        );
+        return parameter ? parameter.values[0] : "-";
+      }
